d8ce83c7c02a41bfd5cc29be8842f7bdcffa58fd
iweizime <i@iweizi.me>
2017-08-12 15:34:28 +0800

    add API version check in DetailsActivity.java

--- app/src/main/java/me/iweizi/wechatstepchanger/DetailsActivity.java
+++ app/src/main/java/me/iweizi/wechatstepchanger/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.icu.util.ChineseCalendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -76,21 +77,37 @@ public class DetailsActivity extends AppCompatActivity {
         }
         setText(R.id.current_today_step_text_view, StepCounterCfg.CURRENT_TODAY_STEP);
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.CHINA);
-        long saveTodayTime = (long) StepCounterCfg.get().getHashMap().get(StepCounterCfg.SAVE_TODAY_TIME);
-        Calendar calendar = ChineseCalendar.getInstance();
+        long saveTodayTime = (long) StepCounterCfg.get().getHashMap()
+                .get(StepCounterCfg.SAVE_TODAY_TIME);
+        StringBuilder builder = new StringBuilder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.CHINA);
+            Calendar calendar = ChineseCalendar.getInstance();
 
-        calendar.setTimeInMillis(saveTodayTime * 10000);
+            calendar.setTimeInMillis(saveTodayTime * 10000);
+            builder.append(dateFormat.format(calendar.getTime()));
+            builder.append("\n");
+        }
+        builder.append(saveTodayTime);
         ((TextView) findViewById(R.id.save_today_time_text_view))
-                .setText(dateFormat.format(calendar.getTime()) + '\n' + String.valueOf(saveTodayTime));
+                .setText(builder.toString());
 
 
         setText(R.id.pre_sensor_step_text_view, StepCounterCfg.PRE_SENSOR_STEP);
 
-        long lastSaveStepTime = (long) StepCounterCfg.get().getHashMap().get(StepCounterCfg.LAST_SAVE_STEP_TIME);
-        calendar.setTimeInMillis(lastSaveStepTime);
+        long lastSaveStepTime = (long) StepCounterCfg.get().getHashMap()
+                .get(StepCounterCfg.LAST_SAVE_STEP_TIME);
+        builder = new StringBuilder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.CHINA);
+            Calendar calendar = ChineseCalendar.getInstance();
+            calendar.setTimeInMillis(lastSaveStepTime);
+            builder.append(dateFormat.format(calendar.getTime()));
+            builder.append("\n");
+        }
+        builder.append(lastSaveStepTime);
         ((TextView) findViewById(R.id.last_save_step_time_text_view))
-                .setText(dateFormat.format(calendar.getTime()) + '\n' + String.valueOf(lastSaveStepTime));
+                .setText(builder.toString());
 
         setText(R.id.sensor_time_stamp_text_view, StepCounterCfg.SENSOR_TIME_STAMP);
 
